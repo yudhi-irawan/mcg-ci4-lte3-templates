95c4b4b403cdf0ac879e600df65966962c70e151
Yudhi Irawan <100767832+yudhi-irawan@users.noreply.github.com>
2022-03-03 15:08:13 +0700

    Create sample_index.php
    sample file that will call the template file

---
+++ app/Views/sample_index.php
@@ -0,0 +1,8 @@
+<?= $this->extend('/templates/012_1st'); ?>
+<?= $this->section('page-content'); ?>
+
+<!-- your html code here! -->
+<h1 class="mt-2">Sample Index</h1>
+
+
+<?= $this->endSection(); ?>
