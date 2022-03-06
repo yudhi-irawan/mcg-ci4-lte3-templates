003da54fee54a0151b569327d145e68900bf456f
Yudhi Irawan <100767832+yudhi-irawan@users.noreply.github.com>
2022-03-06 14:13:48 +0700

    Add files 012.mcg.root.js and lt3.js
    Add files 012.mcg.root.js and lt3.js

---
+++ public/012.mcg.root.js
@@ -0,0 +1,6 @@
+﻿var bootPATH = '/';
+
+//<-- Framework -->
+var js_01 = 'lt3.js';
+
+document.write('<script src="' + bootPATH + js_01 + '" type="text/javascript"></sc' + 'ript>');
---
+++ public/lt3.js
@@ -0,0 +1,128 @@
+﻿//2021-04-09 adminlte-3.2.0 
+var bootPATH = '/';
+
+  //<-- CSS -->  
+var css_01 = "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback";
+var css_02 = "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css";
+var css_03 = "assets/adminlte-3.2.0/plugins/fontawesome-free/css/all.min.css";
+var css_04 = "assets/adminlte-3.2.0/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css";
+var css_05 = "assets/adminlte-3.2.0/plugins/select2/css/select2.min.css";
+var css_06 = "assets/adminlte-3.2.0/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css";
+var css_07 = "assets/adminlte-3.2.0/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css";
+var css_08 = "assets/adminlte-3.2.0/plugins/bs-stepper/css/bs-stepper.min.css";
+var css_09 = "assets/adminlte-3.2.0/plugins/dropzone/min/dropzone.min.css";
+var css_10 = "assets/adminlte-3.2.0/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css";
+var css_11 = "assets/adminlte-3.2.0/plugins/datatables-responsive/css/responsive.bootstrap4.min.css";
+var css_12 = "assets/adminlte-3.2.0/plugins/datatables-buttons/css/buttons.bootstrap4.min.css";
+
+var css_13 = "assets/adminlte-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css";
+var css_14 = "assets/adminlte-3.2.0/plugins/jqvmap/jqvmap.min.css";
+var css_15 = "assets/adminlte-3.2.0/plugins/overlayScrollbars/css/OverlayScrollbars.min.css";
+var css_16 = "assets/adminlte-3.2.0/plugins/daterangepicker/daterangepicker.css";
+var css_17 = "assets/adminlte-3.2.0/plugins/summernote/summernote-bs4.min.css";
+
+//var css_18 = "assets/adminlte-3.2.0/dist/css/adminlte.min.css";
+var css_18 = "assets/adminlte-3.2.0/dist/css/adminlte.css";
+var css_19 = 'assets/my_lib/my_adminlte-3.2.0.css';
+
+
+//var css_20 = 'assets/datatables/css/dataTables.bootstrap.min.css';
+//var css_19 = 'assets/bootstrap-3.4.1-dist/css/bootstrap.min.css';
+//var css_19 = 'assets/bootstrap-4.6.0-dist/css/bootstrap.min.css';
+//var css_22 = 'assets/bootstrap-datepicker/css/bootstrap-datepicker.min.css';
+
+
+
+
+
+//document.write('<link href="' + bootPATH + css_01 + '" rel="stylesheet" type="text/css" />');
+//document.write('<link href="' + bootPATH + css_02 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_03 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_04 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_05 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_06 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_07 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_08 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_09 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_10 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_11 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_12 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_13 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_14 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_15 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_16 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_17 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_18 + '" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + css_19 + '" rel="stylesheet" type="text/css" />');
+
+
+
+
+
+//<-- JS -->
+var js_01 = "assets/adminlte-3.2.0/plugins/jquery/jquery.min.js";
+var js_02 = "assets/adminlte-3.2.0/plugins/jquery-ui/jquery-ui.min.js";
+
+var js_03 = "assets/adminlte-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js";
+var js_04 = "assets/adminlte-3.2.0/plugins/select2/js/select2.full.min.js";
+var js_05 = "assets/adminlte-3.2.0/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js";
+var js_06 = "assets/adminlte-3.2.0/plugins/moment/moment.min.js";
+var js_07 = "assets/adminlte-3.2.0/plugins/inputmask/jquery.inputmask.min.js";
+var js_08 = "assets/adminlte-3.2.0/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js";
+var js_09 = "assets/adminlte-3.2.0/plugins/bootstrap-switch/js/bootstrap-switch.min.js";
+var js_10 = "assets/adminlte-3.2.0/plugins/bs-stepper/js/bs-stepper.min.js";
+var js_11 = "assets/adminlte-3.2.0/plugins/dropzone/min/dropzone.min.js";
+var js_12 = "assets/adminlte-3.2.0/plugins/datatables/jquery.dataTables.min.js";
+var js_13 = "assets/adminlte-3.2.0/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js";
+var js_14 = "assets/adminlte-3.2.0/plugins/datatables-responsive/js/dataTables.responsive.min.js";
+var js_15 = "assets/adminlte-3.2.0/plugins/datatables-responsive/js/responsive.bootstrap4.min.js";
+var js_16 = "assets/adminlte-3.2.0/plugins/datatables-buttons/js/dataTables.buttons.min.js";
+var js_17 = "assets/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.bootstrap4.min.js";
+var js_18 = "assets/adminlte-3.2.0/plugins/jszip/jszip.min.js";
+var js_19 = "assets/adminlte-3.2.0/plugins/pdfmake/pdfmake.min.js";
+var js_20 = "assets/adminlte-3.2.0/plugins/pdfmake/vfs_fonts.js";
+var js_21 = "assets/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.html5.min.js";
+var js_22 = "assets/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.print.min.js";
+var js_23 = "assets/adminlte-3.2.0/plugins/datatables-buttons/js/buttons.colVis.min.js";
+var js_24 = "assets/adminlte-3.2.0/dist/js/adminlte.min.js";
+
+
+/*
+var js_02 = "lt3/plugins/jquery-ui/jquery-ui.min.js";
+
+*/
+
+document.write('<script src="' + bootPATH + js_01 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_02 + '" type="text/javascript"></sc' + 'ript>');
+
+//Resolve conflict in jQuery UI tooltip with Bootstrap tooltip
+document.write('<script>');
+document.write("  $.widget.bridge('uibutton', $.ui.button)");
+document.write('</sc' + 'ript>');
+
+document.write('<script src="' + bootPATH + js_03 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_04 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_05 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_06 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_07 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_08 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_09 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_10 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_11 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_12 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_13 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_14 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_15 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_16 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_17 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_18 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_19 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_20 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_21 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_22 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_23 + '" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + js_24 + '" type="text/javascript"></sc' + 'ript>');
+
+
+
+
